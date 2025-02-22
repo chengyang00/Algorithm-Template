349870ba7b9fb8ae6cf4832b08d686512183e445
Cheng Yang <43635836+chengyang00@users.noreply.github.com>
2025-02-22 14:05:58 +0800

    Update binary_indexed_tree.py

--- complex data structure/binary_indexed_tree.py
+++ complex data structure/binary_indexed_tree.py
@@ -10,14 +10,14 @@ class Fenwick:
     def update(self, i: int, val: int) -> None:
         while i < len(self.f):
             self.f[i] += val
-            i += i & -i
+            i += i & -i  # i & -i = lowbit(i)
 
-    def pre(self, i: int) -> int:
+    def prefixSum(self, i: int) -> int:
         res = 0
         while i > 0:
             res += self.f[i]
-            i &= i - 1
+            i -= i & -i
         return res
 
     def query(self, l: int, r: int) -> int:
-        return self.pre(r) - self.pre(l - 1)
+        return self.prefixSum(r) - self.prefixSum(l - 1)
